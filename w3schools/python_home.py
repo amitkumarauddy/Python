293ee79d9dc1ec6a666e155ad6f914a03d3c277b
amitkumarauddy <amitkumarauddy@gmail.com>
2025-02-18 19:56:54 +0530

    code

---
+++ w3schools/python_home.py
@@ -0,0 +1,56 @@
+print('Hello','World')
+      
+"""
+#x = ("apple", "banana", "cherry") #Is a tuple
+#print(type(x))
+
+#x = {"name" : "John", "age" : 36} # is a dictionary
+#print(type(x))
+
+#x = 5
+#x = complex(x) # The complex() function returns a complex number by specifying a real number and an imaginary number.
+
+#txt = "Hello World"
+#x = txt[2:5] # llo
+
+
+#txt = " Hello World "
+#x = txt.strip() # removes spaces
+
+#txt = "Hello World"
+#txt = txt.replace("H","J") #replcae H with J
+
+If x = 9, what is a correct syntax to print 'The price is 9.00 dollars'?
+print(f'The price is {x:.2f} dollars') #decimal specifier
+ 
+
+age = 36
+txt = f"My name is John, and I am {age}"
+print(txt)
+
+fruits = ['apple', 'banana', 'cherry']
+tropical = ['mango', 'pineapple', 'papaya']
+fruits.extend(tropical) @ add elements of tropical to fruits
+
+fruits = ['apple', 'banana', 'cherry']
+fruits.delete() #removes all elements
+
+mylist = ['apple', 'banana', 'cherry']
+i = 0
+while
+ i < len(mylist):
+     print(mylist[i])
+     i = i + 1
+
+[print(x) for x in ['apple', 'banana', 'cherry']] #printing the loop
+
+fruits = ['apple', 'banana', 'cherry']
+newlist = ['apple' for x in fruits] # ['apple', 'apple', 'apple']
+
+
+fruits = ['apple', 'banana', 'cherry']
+newlist = [x for x in fruits if x == 'banana']
+print(newlist)
+
+mylist.sort(reverse = True) # sorting in descending order
+"""
