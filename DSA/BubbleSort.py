8c332c2d09b1432b52a455b5e9f76c54bdce7720
amitkumarauddy <amitkumarauddy@gmail.com>
2025-03-03 23:43:43 +0530

    code

---
+++ DSA/BubbleSort.py
@@ -0,0 +1,15 @@
+a = []
+
+n = int((input("Enter the total number of elements: ")))
+
+for i in range(n):
+    element = int(input(f"Enter element [{i+1}] to be bubble sorted: "))
+    a.append(element)
+
+temp = 0
+for i in range(n):
+    for j in range(n-1):
+        if a[j] > a[j+1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+
+print(a)
